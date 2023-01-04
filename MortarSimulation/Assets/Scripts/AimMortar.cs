f9424e7a0b090fc212ef7ef06025c1765e49a104
dalen.grdic <dalen.grdic@fer.hr>
2023-01-04 12:51:33 +0100

    Show barrel angle

--- MortarSimulation/Assets/Scripts/AimMortar.cs
+++ MortarSimulation/Assets/Scripts/AimMortar.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AimMortar : MonoBehaviour
 {
     
     [SerializeField]
     private float sensitivity = 0.1f;
+
+    [SerializeField]
+    public TextMeshProUGUI text;
+
+    void start() {
+        text.text = "Angle " + System.Math.Round(360 - transform.rotation.eulerAngles.x, 2);
+    }
 
     // rotates in range from 280 to 330 degrees
     void Update()
@@ -15,11 +23,13 @@ public class AimMortar : MonoBehaviour
             if(transform.rotation.eulerAngles.x >= 280) {
                 Quaternion step = Quaternion.Euler(-sensitivity, 0, 0);
                 transform.rotation = transform.rotation * step;
+                text.text = "Angle " + System.Math.Round(360 - transform.rotation.eulerAngles.x, 2);
             }
         } else if (Input.GetKey ("down")) {
             if(transform.rotation.eulerAngles.x <= 330) {
                 Quaternion step = Quaternion.Euler(sensitivity, 0, 0);
                 transform.rotation = transform.rotation * step;
+                text.text = "Angle " + System.Math.Round(360 - transform.rotation.eulerAngles.x, 2);
             }
         }
     }
