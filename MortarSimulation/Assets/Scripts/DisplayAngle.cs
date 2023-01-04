80542e537fec11efb4787f0e67e3a5825c9b8c8e
dalen.grdic <dalen.grdic@fer.hr>
2023-01-04 19:15:53 +0100

    Fixed angle display, tank spawning logic and reset option

---
+++ MortarSimulation/Assets/Scripts/DisplayAngle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayAngle : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMeshProUGUI textMesh;
+
+    private GameObject mortarBarrel;
+    
+    void Update()
+    {
+        mortarBarrel = GameObject.FindWithTag("BarrelEnd");
+        if(mortarBarrel != null) {
+            textMesh.text = "Angle: " + System.Math.Round(360 - mortarBarrel.transform.rotation.eulerAngles.x, 2);
+        }
+    }
+}
--- MortarSimulation/Assets/Scripts/SpawnerScript.cs
+++ MortarSimulation/Assets/Scripts/SpawnerScript.cs
@@ -5,37 +5,59 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
 
-    private Vector3 spawnPosition;
+    private Vector3 tankSpawnPosition;
+    private Vector3 spawnPosition = new Vector3(0, 0, -100);
     private Vector3 rotateTo = new Vector3(0, 0, -10);
     public GameObject tankPrefab;
+    public GameObject mortarCrewPrefab;
     private float spawnRate;
+    private GameObject left;
+    private GameObject right;
+    private GameObject mortarCrew;
+
+    // spawn rate
+    private float cooldownTime = 10f;
 
     [SerializeField]
     public float spawnDistance = 120f;
     // Start is called before the first frame update
     void Start()
     {
-        spawnRate = -10f;
+        // for initial spawn, 10s cooldown is applied afterwards
+        spawnRate = -cooldownTime;
+
+        // spawn mortar crew on scene load
+        mortarCrew = Instantiate(mortarCrewPrefab, spawnPosition, Quaternion.identity);
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
 
+        // calculate passed time
         spawnRate -= Time.deltaTime;
 
+        // if space is pressed and cooldown time expired, attempt to spawn new tank object
         if(Input.GetKey("space") && spawnRate < 0f) {
             float random = Random.Range(-1f, 1f);
 
-            if(random > 0) {
-                spawnPosition = new Vector3(spawnDistance, 0, -10);
-                Instantiate(tankPrefab, spawnPosition, Quaternion.Euler(0, -90, 0));
-            } else {
-                spawnPosition = new Vector3(-spawnDistance, 0, -10);
-                Instantiate(tankPrefab, spawnPosition, Quaternion.Euler(0, 90, 0));
+            // make sure tanks are not already instantiated on this side
+            if(random > 0 && right == null) {
+                tankSpawnPosition = new Vector3(spawnDistance, 0, -10);
+                right =  Instantiate(tankPrefab, tankSpawnPosition, Quaternion.Euler(0, -90, 0));
+            } else if(random < 0 && left == null) {
+                tankSpawnPosition = new Vector3(-spawnDistance, 0, -10);
+                left = Instantiate(tankPrefab, tankSpawnPosition, Quaternion.Euler(0, 90, 0));
             }
 
-            spawnRate = 10f;
+            // set cooldown time back;
+            spawnRate = cooldownTime;
+
+        } else if(Input.GetKeyDown(KeyCode.R)) { // reset scene
+            Destroy(left);
+            Destroy(right);
+            Destroy(mortarCrew);
+            mortarCrew = Instantiate(mortarCrewPrefab, spawnPosition, Quaternion.identity);
         }
     }
 }
